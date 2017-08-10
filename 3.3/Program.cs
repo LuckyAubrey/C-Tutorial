7839f38665e13b21865c98990bc7f09bf804ee66
Zieluowaty <zielu.yabi@gmail.com>
2017-08-10 23:30:54 +0200

    Sformatowano kod.

--- 3.3/Program.cs
+++ 3.3/Program.cs
@@ -13,7 +13,7 @@ namespace _3._3
             string text = Console.ReadLine();
             double.TryParse(text, out waga);
             if (waga <= 0)
-                {
+            {
                 Console.Write("Podaj wage w kg: ");
                 text = Console.ReadLine();
                 double.TryParse(text, out waga);
@@ -25,7 +25,7 @@ namespace _3._3
 
             wskaznikBMI = waga / Math.Pow(wzrost, 2.0);
             czyWagaNorma = wskaznikBMI >= 18.5 && wskaznikBMI < 25;
-            Console.WriteLine("BMI = {0,6:F2}", wskaznikBMI );
+            Console.WriteLine("BMI = {0,6:F2}", wskaznikBMI);
             Console.WriteLine("Czy jestes gruba swinia?");
             if (czyWagaNorma == false)
             {
@@ -35,7 +35,7 @@ namespace _3._3
             {
                 Console.Write("Nie, chudzielcu!");
             }
-                Console.ReadKey();
-            }
+            Console.ReadKey();
+        }
     }
 }
